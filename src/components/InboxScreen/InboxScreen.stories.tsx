788c6f2590e7195a1b4eb0250a785e468c94bb90
Alcir Junior <junior@cajucomunica.com.br>
2021-06-15 11:29:16 -0300

    feat: Add InboxScreen Component

---
+++ src/components/InboxScreen/InboxScreen.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Provider } from 'react-redux';
+
+import { InboxScreen } from '.';
+import { defaultTasksData } from '../TaskList/TaskList.stories';
+
+export default {
+  component: InboxScreen,
+  title: 'InboxScreen',
+  decorators: [(story: any) => <Provider store={store}>{story()}</Provider>]
+};
+
+const store = {
+  getState: () => {
+    return {
+      tasks: defaultTasksData
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action('dispatch')
+};
+
+export const Default = () => <InboxScreen />;
+export const Error = () => <InboxScreen error="Something" />;
