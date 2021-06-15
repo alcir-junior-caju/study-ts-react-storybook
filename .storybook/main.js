8bb1f7c557c1c7f07ea59802505e664c571ff81e
Alcir Junior <junior@cajucomunica.com.br>
2021-06-15 14:32:24 -0300

    feat: Add knobs on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
 module.exports = {
   stories: ["../src/components/**/*.stories.tsx"],
   addons: [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-knobs",
+    "@storybook/addon-links",
     {
       name: "@storybook/addon-docs",
       options: {
